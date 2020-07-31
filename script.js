b56529843032029fcf23e4c90c445905f2e914a9
Alex D. Guzman <alexd.guzman98@gmail.com>
2020-07-30 19:23:24 -0600

    tarea dom

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ function mix() {
      y = Math.floor(Math.random() * 256);
      z = Math.floor(Math.random() * 256);
         
-    return `rgb(${x},${y},${z})`
+    return `rgb(${x},${y},${z})`;
 }
 let parrafo = document.querySelectorAll('.container p');
 for(let i = 0; i < parrafo.length; i++){
     parrafo[i].style.color = mix();
- 
 }
 console.log(mix());
